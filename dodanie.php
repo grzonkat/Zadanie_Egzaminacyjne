a0ac0fd992a5e98f789ee617ded365cf67b4236c
Grzonka Tymoteusz <grzonka.tymoteusz@zs6sobieski.edu.pl>
2024-02-27 14:39:55 +0100

    zdjecia git oraz naprawa pliku

--- dodanie.php
+++ dodanie.php
@@ -1,10 +1,11 @@
 <?php
-    $con = mysqli_connect('localhost','root',' ','ee09');
-    @$nrKaretki=$_POST['nrKaretki'];
-    @$ratownik1=$_POST['ratownik1'];
-    @$ratownik2=$_POST['ratownik2'];
-    @$ratownik3=$_POST['ratownik3'];
-    $zap = mysqli_query("INSERT INTO `ratownicy` ( `nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ('$nrKaretki','$ratownik1','$ratownik2','$ratownik3');");
-    echo "Do bazy zostało wysłane zapytanie: ".$zap;
+    $con=mysqli_connect("localhost","root","","ee09");
+    $karetka=$_POST['nrKaretki'];
+    $ratownik1=$_POST['ratownik1'];
+    $ratownik2=$_POST['ratownik2'];
+    $ratownik3=$_POST['ratownik3'];
+    $zap="INSERT INTO ratownicy(nrKaretki,ratownik1,ratownik2,ratownik3) VALUES ('$karetka','$ratownik1','$ratownik2','$ratownik3');";
+    $query=mysqli_query($con,$zap);
+    echo'Do bazy zostało wysłane zapytanie: INSERT INTO ratownicy (nrKaretki,ratownik1,ratownik2,ratownik3) VALUES ('.$karetka.','.$r1.','.$r2.','.$r3.');';
     mysqli_close($con);
 ?>
